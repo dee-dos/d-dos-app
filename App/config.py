544cd231c8a28fbfbe8856c253f4ebefbfdba7c3
BreretonCodez <53999272+BreretonCodez@users.noreply.github.com>
2022-09-26 00:40:08 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgres://sojklksywbsgje:cb595d3bac82c033e6fda0e4d94e7af62ed7a8cf287d2c431536f88ee86c15a8@ec2-44-205-63-142.compute-1.amazonaws.com:5432/dc48uup31qb12r"
+SQLALCHEMY_DATABASE_URI = "postgresql://sojklksywbsgje:cb595d3bac82c033e6fda0e4d94e7af62ed7a8cf287d2c431536f88ee86c15a8@ec2-44-205-63-142.compute-1.amazonaws.com:5432/dc48uup31qb12r"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "production"
