bb60eecf9700677712fd989ee0db68a8beef2914
BreretonCodez <53999272+BreretonCodez@users.noreply.github.com>
2022-10-29 16:04:58 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://vcayxxbvdsbgni:f66d6fcefa82f71768ae4e72b9d1580b1a314e405662e54bbaa845efefc77662@ec2-54-157-74-211.compute-1.amazonaws.com:5432/d6timhsbcgo44r"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
-ENV = "DEVELOPMENT"
+ENV = "production"
