89b1f80993bc634fe9c65c8741f4ab12f86daa51
BreretonCodez <micahbrereton@hotmail.com>
2022-11-01 13:44:42 +0000

    Update publication model

--- App/models/publication.py
+++ App/models/publication.py
@@ -5,16 +5,17 @@ from .model import *
 class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    author = db.Column(db.ForeignKey('author.id'), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
     coauthors = db.relationship('Author', secondary=pub_tree)
     content = db.Column(db.String, nullable=False)
     citation = db.Column(db.String, nullable=False)
 
     def toJSON(self):
+        man = self.author.toJSON
         return {
             'id': self.id,
             'name': self.name,
-            'author': self.author.toJSON,
+            'author': man,
             'co-authors': self.coauthors,
             'content': self.content,
             'citation': self.citation
